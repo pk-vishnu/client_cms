f4594cf9b010d5f5309c671b3f26626c09cf7261
Vishnu P K <vishnuvpk05@gmail.com>
2024-07-30 22:57:18 +0530

    feat: preview client website

--- website/views.py
+++ website/views.py
@@ -161,3 +161,13 @@ def delete_image(id):
 @login_required
 def product():
     return render_template("product.html",user=current_user)
+
+@views.route('/preview_site')
+@login_required
+def preview_site():
+    return render_template("preview_home.html",user=current_user)
+
+@views.route('/preview_products')
+@login_required
+def preview_products():
+    return render_template("preview_products.html",user=current_user)
